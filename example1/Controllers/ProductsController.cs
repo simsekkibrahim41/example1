236e0cfe698898000c466d89d97d2bc057e96f0a
Dadaş <Dada@DESKTOP-BQT9OT7>
2022-08-29 17:50:34 +0300

    Add project files.

---
+++ example1/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace example1.Controllers
+{
+
+    [Route("Products")]
+    public class ProductsController:ControllerBase
+    {
+        [Route("Merhaba")]
+        public string Merhaba()
+        {
+            return "merhaba";
+
+        }
+
+        [Route("Hello")]
+        public string Hello()
+        {
+            return "hello";
+
+        }
+
+    }
+}
